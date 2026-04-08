759b36253221aa0b8c4fd71fae559a1f1bed263b
anthonipineda <ypinedaj@my.gvltec.edu>
2026-04-08 13:23:31 -0400

    Guess Row V2
    An updated functionality that also controls how the keyboard gets updated

--- GuessRow.java
+++ GuessRow.java
@@ -1,144 +1,117 @@
-// NOTE: Remember to build this and any other class!!
-
-// Game: Wordle (GuessRow class)
-
-// Anthoni Pineda
-// Justice Earl Pinkney
-// Zachary Ross
-
-// CPT-237-W34
-// Spring 2026
-
-
 import javafx.geometry.*;
 import javafx.scene.layout.*;
+import javafx.animation.PauseTransition;
+import javafx.util.Duration;
 
 public class GuessRow extends HBox
 {
-   Insets letterMargins = new Insets(5.0, 5.0, 5.0, 5.0);
-   
    public String guess = "";
-   
-   // Constructors
-   GuessRow()
+   GroupProjectWordle parent;
+
+//Constructor
+   GuessRow(GroupProjectWordle parent)
    {
+      this.parent = parent;
       this.setAlignment(Pos.CENTER);
-      
-      // Adding 5 LetterTile as children
+
       for(int i=0; i<5; i++)
       {
-         LetterTile newTile = new LetterTile();
-         this.getChildren().add(newTile);
-         this.setMargin(newTile, letterMargins);
+         this.getChildren().add(new LetterTile());
       }
    }
-   
+
+// resets rows
    public void reset()
    {
       guess = "";
-      for(int i=0; i<5; i++)
+      for (int i=0;i<5;i++)
       {
-         LetterTile selectedTile = (LetterTile)this.getChildren().get(i);
-         selectedTile.unselectLetter();
-         selectedTile.setLetter("");
+         LetterTile t = (LetterTile)getChildren().get(i);
+         t.setLetter("");
+         t.setDefault();
       }
    }
-   
+//adds selection indication
    public void nowGuessing()
    {
-      // first tile is selected
-      LetterTile selectedTile = (LetterTile)this.getChildren().get(0);
-      selectedTile.selectLetter();
+      ((LetterTile)getChildren().get(0)).selectLetter();
    }
-   
+//adds letter
    public void addLetterToGuess(String letter)
    {
-      if (guess.length() == 5) return; // If guess is full, do nothing.
-      
-      // Updating tile the letter is going in.
-      LetterTile selectedTile = (LetterTile)this.getChildren().get(guess.length());
-      selectedTile.setLetter(letter);
-      selectedTile.unselectLetter();
-      
-      guess += letter; // Add the letter to guess
-      
-      if (guess.length() == 5) return; // If guess is full, stop.
-      
-      // Guess isn't full, highlight next letter tile
-      selectedTile = (LetterTile)this.getChildren().get(guess.length());
-      selectedTile.selectLetter();
+      if (guess.length() == 5) return;
+
+      LetterTile t = (LetterTile)getChildren().get(guess.length());
+      t.setLetter(letter);
+      t.unselectLetter();
+
+      guess += letter;
+
+      if (guess.length() < 5)
+         ((LetterTile)getChildren().get(guess.length())).selectLetter();
    }
-   
+//removes letter
    public void backspace()
    {
-      if (guess.length() == 0) return; // If guess is already empty, do nothing.
-      
-      LetterTile selectedTile;
-      if (guess.length() < 5)
-      {
-         // Reversing select
-         selectedTile = (LetterTile)this.getChildren().get(guess.length());
-         selectedTile.unselectLetter();
-      }
-      
-      guess = guess.substring(0, guess.length() - 1); // Truncate last letter
-      
-      // Highlighting previous letter tile
-      selectedTile = (LetterTile)this.getChildren().get(guess.length());
-      selectedTile.setLetter("");
-      selectedTile.selectLetter();
+      if (guess.length() == 0) return;
+
+      guess = guess.substring(0, guess.length()-1);
+
+      LetterTile t = (LetterTile)getChildren().get(guess.length());
+      t.setLetter("");
+      t.selectLetter();
    }
-   
-   public boolean adjudicateGuess(String answer) //edited to use a two pass system so words with duplicate letters can be marked correctly
-{
-    answer = answer.toUpperCase();
-    guess = guess.toUpperCase();
 
-    // Track which letters in the answer have been matched
-    boolean[] used = new boolean[5];
+   public boolean adjudicateGuess(String answer)
+   {
+      boolean[] used = new boolean[5];
 
-    // Pass 1 mark green tiles
-    for (int i = 0; i < 5; i++)
-    {
-        LetterTile tile = (LetterTile) this.getChildren().get(i);
+      for (int i=0;i<5;i++)
+      {
+         int index = i;
+//adds small effect
+         PauseTransition pause = new PauseTransition(Duration.millis(300*i));
+         pause.setOnFinished(e -> {
 
-        if (guess.charAt(i) == answer.charAt(i))
-        {
-            tile.setCorrect();
-            used[i] = true;  // mark this answer position as used
-        }
-    }
+            LetterTile tile = (LetterTile)getChildren().get(index);
+            char g = guess.charAt(index);
 
-    // Pass 2 mark yellow and gray tiles
-    for (int i = 0; i < 5; i++)
-    {
-        LetterTile tile = (LetterTile) this.getChildren().get(i);
-
-        // Skip green tiles
-        if (guess.charAt(i) == answer.charAt(i))
-            continue;
-
-        boolean found = false;
-
-        // Look for an unused matching letter in the answer
-        for (int j = 0; j < 5; j++)
-        {
-            if (!used[j] && guess.charAt(i) == answer.charAt(j))
+            if (g == answer.charAt(index))
             {
-                found = true;
-                used[j] = true;  // mark this answer position as used
-                break;
+               tile.setCorrect();
+               parent.updateKeyboardColor(""+g,"green");
+               used[index] = true;
             }
-        }
+            else
+            {
+               boolean found = false;
 
-        if (found)
-            tile.setMisplaced();  // yellow
-        else
-            tile.setIncorrect();  // gray
-    }
+               for (int j=0;j<5;j++)
+               {
+                  if (!used[j] && g == answer.charAt(j))
+                  {
+                     found = true;
+                     used[j] = true;
+                     break;
+                  }
+               }
 
-    // Win condition
-    return guess.equals(answer);
-}
+               if (found)
+               {
+                  tile.setMisplaced();
+                  parent.updateKeyboardColor(""+g,"yellow"); //updates keyboard for misplaced
+               }
+               else
+               {
+                  tile.setIncorrect();
+                  parent.updateKeyboardColor(""+g,"gray"); //updates keyboard for incorrect
+               }
+            }
+         });
 
+         pause.play();
+      }
+
+      return guess.equals(answer);
+   }
 }
