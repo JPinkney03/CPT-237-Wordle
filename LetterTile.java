4c358f2a98b0180be7fdf2c42ee0a925171ad024
anthonipineda <ypinedaj@my.gvltec.edu>
2026-04-22 13:46:39 -0400

    Update LetterTile.java

--- LetterTile.java
+++ LetterTile.java
@@ -5,38 +5,44 @@ import javafx.geometry.*;
 public class LetterTile extends StackPane
 {
    Label letterDisplay;
-
+   
+//Creates a letter tile, constructor
    LetterTile()
    {
       letterDisplay = new Label("");
       letterDisplay.getStyleClass().add("tile");
       letterDisplay.setMinSize(50,50);
       letterDisplay.setAlignment(Pos.CENTER);
-  letterDisplay.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
-  
+      letterDisplay.setStyle("-fx-font-size: 24px; -fx-font-weight: bold;");
+   
       getChildren().add(letterDisplay);
    }
-
+   
+//changes letterdisplay to the letter typed
    public void setLetter(String letter)
    {
       letterDisplay.setText(letter);
    }
-
+   
+//shows which lettertile is selected
    public void selectLetter()
    {
       setStyle("-fx-border-color: blue;");
    }
-
+   
+//unselects the lettertile
    public void unselectLetter()
    {
       setStyle("-fx-border-color: black;");
    }
 
+//resets lettertile to default
    public void setDefault()
    {
       setStyle("-fx-background-color: white; -fx-border-color: black;");
    }
 
+//The following modifies tile based on guess state
    public void setCorrect()
    {
       setStyle("-fx-background-color: #188754;");
